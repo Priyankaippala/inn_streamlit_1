715835ed93fede5e7d2948131004a6b423a79437
Priyankaippala <114237847+Priyankaippala@users.noreply.github.com>
2023-02-26 21:29:35 +0530

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -1,2 +1,5 @@
 import streamlit as st 
-st.header('hello')
+st.title(':red[Hello!]:wave:... I\'m Priyanka Ippala')
+st.subheader('Let me tell u something....I made this entire webpage Just by using Streamlit.')
+st.subheader('Is\'nt it cool to design such a page with just using  a library of pyhton...:smile:')
+st.caption('Visit sripts.io page to learn more')
